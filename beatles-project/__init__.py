879dbe3d3dcc03e2e6d8561852ca522c5a1423ca
GrindelfP <grindelf.perlomutrovij@gmail.com>
2023-10-13 10:05:12 +0300

    init

---
+++ beatles-project/__init__.py
@@ -0,0 +1,6 @@
+information: str = "Beatles Project. Imitational and math models of movement of four beetles."
+
+
+def info():
+    """Prints information about the module."""
+    print(information)
---
+++ cup-of-coffee/__init__.py
@@ -0,0 +1,6 @@
+information: str = "Cup of Coffee. Mathmodel of cup of coffee temperature dropping down as time passes."
+
+
+def info():
+    """Prints information about the module."""
+    print(information)
