18c6bc02f5450a9283449978f476e99a23f9532e
GrindelfP <grindelf.perlomutrovij@gmail.com>
2023-11-16 23:50:51 +0300

    made row task 3

---
+++ src/3-predator-prey/__init__.py
@@ -0,0 +1,9 @@
+information: str = ("Predator-Prey model. Mathmodel of interacting "
+                    "populations of predator and prey species.")
+
+
+def info():
+    """
+    Prints information about the module.
+    """
+    print(information)
